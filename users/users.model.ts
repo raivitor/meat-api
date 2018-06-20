9055b6a37614ca5627d06711c13b84c93ba017fb
Rai Vitor <raivitor@gmail.com>
2018-06-19 22:05:35 -0300

    Recebendo os dados do banco.

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,22 +1,17 @@
+import * as mongoose from 'mongoose'
 
-const users = [
-  {id: '1', name:'Peter Parker', email: 'peter@marvel.com'},
-  {id: '2', name:'Bruce Wayne', email: 'bruce@dc.com'}
-]
-
-export class User {
-  static findAll(): Promise<any[]>{
-    return Promise.resolve(users)
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    select: false
   }
+})
 
-  static findById(id: string): Promise<any>{
-    return new Promise(resolve=>{
-      const filtered = users.filter(user=> user.id === id)
-      let user = undefined
-      if(filtered.length > 0){
-        user = filtered[0]
-      }
-      resolve(user)
-    })
-  }
-}
+export const User = mongoose.model('User', userSchema)
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,20 +1,20 @@
-import {Router} from '../common/router'
+import { Router } from '../common/router'
 import * as restify from 'restify'
-import {User} from './users.model'
+import { User } from './users.model'
 
 class UsersRouter extends Router {
-  applyRoutes(application: restify.Server){
+  applyRoutes(application: restify.Server) {
 
-    application.get('/users', (req, resp, next)=>{
-      User.findAll().then(users=>{
+    application.get('/users', (req, resp, next) => {
+      User.find().then(users => {
         resp.json(users)
         return next()
       })
     })
 
-    application.get('/users/:id', (req, resp, next)=>{
-      User.findById(req.params.id).then(user=>{
-        if(user){
+    application.get('/users/:id', (req, resp, next) => {
+      User.findById(req.params.id).then(user => {
+        if (user) {
           resp.json(user)
           return next()
         }
